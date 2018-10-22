61fc048646164e41eabdf1266e54b6a7dee9f14e
hamza <hamza95.hw@gmail.com>
2018-10-22 18:37:16 +0300

    seeds.rb file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 admin = User.new(
-    :email => "ha@gmail.com",
+    :email => "ha@gmail.com" "alongside@gmail.com",
     :password => "123456"
 )
 if admin.save
